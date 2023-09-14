1c25b296b3422e3eac189bb35190425c7b5a0404
rafaelanchieta <lynux@gmail.com>
2023-09-14 16:10:59 -0300

    refactored init

--- init.py
+++ init.py
@@ -1,39 +1,48 @@
+import re
 import pandas as pd
-import os, re
 
-import numpy as np
-import matplotlib as plt
-
-from boolean_features import BooleanFeatures
+from features import Features
 from similarity import cosine_similarity
+from util import read_notazero, read_prompts
 
-def read_corpus_and_prompts():
-    path = 'essays'
-    essays = pd.read_csv(os.path.join(path, 'essay-br.csv'), converters={'essay': eval, 'competence': eval})
-    prompts = pd.read_csv(os.path.join(path, 'prompts.csv'))
-    return essays, prompts
 
 if __name__ == "__main__":
-    essays, prompts = read_corpus_and_prompts()
-    boolean_features = BooleanFeatures()
+    # dataframes para os aruivos essay-br e prompts no diretório essays
+    notas_zero = read_notazero()
+    prompts = read_prompts()
 
-    topic = essays['prompt'][0]
-    result = prompts[prompts['id'] == topic]
-    regex = r"'(.*?)'"
-    for paragraph in result['description']:
-        paragraph = paragraph.replace('[', '').replace(']', '')
-        topic_paragraphs = re.findall(regex, paragraph)
-    
-    similarities = []  # Lista para armazenar as similaridades
-    
-    for p in topic_paragraphs:
-        for essay in essays['essay'][0]:
-            snt1_vec, snt2_vec = boolean_features.sent_to_vec(p, essay)
-            similarity = cosine_similarity(snt1_vec, snt2_vec)
-            similarities.append(similarity)
+    # Instancia a classe Features responsável por transformar uma sentença em um vetor
+    # type=0 -> boolean features
+    # type=1 -> TF features
+    # type=2 -> TF-IDF features
+    features = Features(type=2)
+
+    # Itera sobre todas as linhas do arquivo notazero
+    for index, row in notas_zero.iterrows():
+        # pega o tópico das redações
+        prompt_id = row['prompt']
+
+        # mapeia o id do arquivo notazero com o id do arquivo prompts
+        prompt = prompts[prompts['id'] == prompt_id]
+
+        # cria uma lista de parágrafos (texto motivacional) da coluna description do arquivo prompts
+        regex = r"'(.*?)'"
+        for paragraph in prompt['description']:
+            paragraph = paragraph.replace('[', '').replace(']', '')
+            topic_paragraphs = re.findall(regex, paragraph)
+
+        similarities = []  # Lista para armazenar as similaridades
+
+        # calcula a similaridade cada parágrafo do texto motivacional com cada parágrafo da redação
+        for p in topic_paragraphs:
+            for paragraphs_essay in notas_zero['essay']:
+                for paragraph_essay in paragraphs_essay:
+                    snt1_vec, snt2_vec = features.sent_to_vec(p, paragraph_essay)
+                    similarity = cosine_similarity(snt1_vec, snt2_vec)
+                    similarities.append(similarity)
     
     # Criar um DataFrame com os resultados
     df_results = pd.DataFrame({'Similarity': similarities})
     
-    # Salvar o DataFrame em um arquivo Excel
-    df_results.to_excel('resultadosinit.xlsx', index=False)
+    # Salvar o DataFrame em um arquivo CSV
+    df_results.to_csv('resultadosinit.csv', index=False)
