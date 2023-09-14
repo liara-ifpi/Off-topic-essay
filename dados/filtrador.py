e067336c59d4032b5fb8d504722cd42e1ab34d84
rafaelanchieta <lynux@gmail.com>
2023-09-14 16:05:41 -0300

    filtrador (refatorado)

--- dados/filtrador.py
+++ dados/filtrador.py
@@ -1,19 +1,20 @@
 import pandas as pd
-import numpy as np
-import matplotlib as plt
-
-#imporcao de bibliotecas
 
 
-tabela = pd.read_excel("essay-br.xlsx", engine='openpyxl') #variavel recebe a planilha junto com outra biblioteca para leitura e modificaçao
+# importacao de bibliotecas
+
+
+# tabela = pd.read_excel("essay-br.xlsx", engine='openpyxl') # variavel recebe a planilha junto com outra biblioteca para leitura e modificaçao
+tabela = pd.read_csv("../essays/essay-br.csv", converters={'essay': eval, 'competence': eval}) # variavel recebe a planilha junto com outra biblioteca para leitura e modificaçao
 
 filtrados = tabela['score'] == 0 # a variavel filtrados recebe a tabela com parametros 
 
 
 pd.get_option('display.max_rows')
-pd.set_option('display.max_row', 100) #aumenta o numero de colunas a serem visualisadas
+pd.set_option('display.max_row', 100) # aumenta o numero de colunas a serem visualisadas
 
 print("dados filtrados:")
 print(tabela[filtrados])
 
-tabela[filtrados].to_excel('filtrados.xlsx', index=False) #salva e cria o artivro em xlsx
+# tabela[filtrados].to_excel('filtrados.xlsx', index=False) # salva e cria o artivro em xlsx
+tabela[filtrados].to_csv('notazero.csv', index=False) # salva e cria o artivro em csv
